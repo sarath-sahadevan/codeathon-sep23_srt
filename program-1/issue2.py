e78f746bc2b98d4855a1afa8ad8eded328af3676
Sarath Sahadevan <sarath.sahadevan@airindia.com>
2023-09-21 15:05:00 +0530

    issue 2

--- program-1/issue2.py
+++ program-1/issue2.py
@@ -15,3 +15,25 @@ def max_profit(prices):
         return -1
     else:
         return max_profit
+    
+
+import unittest
+
+class TestMaxProfit(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertEqual(max_profit([]), -1)
+    
+    def test_single_price(self):
+        self.assertEqual(max_profit([5]), -1)
+    
+    def test_no_profit(self):
+        self.assertEqual(max_profit([7, 6, 5, 4, 3, 2, 1]), -1)
+    
+    def test_max_profit(self):
+        self.assertEqual(max_profit([7, 1, 5, 3, 6, 4]), 5)
+    
+    def test_duplicate_prices(self):
+        self.assertEqual(max_profit([7, 1, 5, 5, 3, 6, 4]), 5)
+    
+if __name__ == '__main__':
+    unittest.main()
